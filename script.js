bfdf481971072a50a11a77e9b79fa12502a10d81
otipz19 <tasitwork@gmail.com>
2024-05-29 14:02:12 +0300

    Added localStorage data saving

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+var PRODUCT_TO_DATA_LOCAL_STORAGE_KEY = "productNameToData";
+var ID_TO_PRODUCT_LOCAL_STORAGE_KEY = "productIdToName";
+if (localStorage.getItem(PRODUCT_TO_DATA_LOCAL_STORAGE_KEY) == null) {
+    localStorage.setItem(PRODUCT_TO_DATA_LOCAL_STORAGE_KEY, "[]");
+}
+if (localStorage.getItem(ID_TO_PRODUCT_LOCAL_STORAGE_KEY) == null) {
+    localStorage.setItem(ID_TO_PRODUCT_LOCAL_STORAGE_KEY, "[]");
+}
+readProductIdMap().forEach(function (name, id) {
+    var data = getProductDataByName(name);
+    createProduct(id, data);
+});
 var productAddForm = document.querySelector("form.create-product-bar");
 productAddForm.addEventListener("submit", function (event) {
     event.preventDefault();
     var productName = popProductName();
     if (productName != null) {
-        createProduct(productName);
+        if (!checkIfProductExist(productName)) {
+            var productId = generateProductId();
+            setProductNameById(productId, productName);
+            var productData = { name: productName, amount: 1, isBought: false };
+            setProductDataByName(productName, productData);
+            createProduct(productId, productData);
+        }
+        else {
+            alert("\u0422\u043E\u0432\u0430\u0440 \u0437 \u043D\u0430\u0437\u0432\u043E\u044E ".concat(productName, " \u0432\u0436\u0435 \u0456\u0441\u043D\u0443\u0454!"));
+        }
     }
 });
-["Помідори", "Печиво", "Сир"].forEach(function (name) { return createProduct(name); });
-document.querySelector(".buy-btn").click();
 function popProductName() {
     var input = productAddForm === null || productAddForm === void 0 ? void 0 : productAddForm.getElementsByTagName("input")[0];
     if (input.value == "") {
@@ -17,22 +36,27 @@ function popProductName() {
     input.value = "";
     return productName;
 }
-function createProduct(productName) {
-    var productId = generateProductId(productName);
-    createProductsListItem(productName, productId);
-    createStatisticsItem(productName, productId);
+function createProduct(productId, productData) {
+    var productItemSection = createProductsListItem(productData, productId);
+    createStatisticsItem(productData, productId);
+    if (productData.isBought) {
+        var buyBtn = productItemSection.querySelector(".buy-btn");
+        buyBtn.click();
+    }
+    updateAmount(productId, productItemSection, function (amount) { return productData.amount; });
 }
-function generateProductId(productName) {
-    return "".concat(productName, "-").concat(Math.random());
+function generateProductId() {
+    return Math.random().toString();
 }
-function createProductsListItem(productName, productId) {
+function createProductsListItem(productData, productId) {
     var productItemSection = getProductItemFromTemplate();
     setProductId(productItemSection, productId);
-    setupProductTitle(productItemSection, productName, productId);
+    setupProductTitle(productItemSection, productData.name, productId);
     setupCancelBtn(productItemSection, productId);
     setupBuyBtn(productItemSection, productId);
-    setupAmountBtns(productItemSection, productId);
+    setupAmountBtns(productItemSection, productId, productData.amount);
     addProductItemToList(productItemSection);
+    return productItemSection;
 }
 function setupProductTitle(productItemSection, productName, productId) {
     var productTitle = productItemSection.querySelector("input.product-title");
@@ -40,10 +64,23 @@ function setupProductTitle(productItemSection, productName, productId) {
     productTitle.addEventListener("change", function () {
         var statisticsItem = getStatisticsItem(productId);
         var name = statisticsItem.firstElementChild;
-        if (productTitle.value != "") {
-            name.innerText = productTitle.value;
+        if (!checkIfProductExist(productTitle.value)) {
+            if (productTitle.value != "") {
+                var newName = productTitle.value;
+                var oldName = productName;
+                setProductNameById(productId, newName);
+                var productData = getProductDataByName(oldName);
+                deleteProductDataByName(oldName);
+                productData.name = newName;
+                name.innerText = newName;
+                setProductDataByName(productData.name, productData);
+            }
+            else {
+                productTitle.value = name.innerText;
+            }
         }
         else {
+            alert("\u0422\u043E\u0432\u0430\u0440 \u0437 \u043D\u0430\u0437\u0432\u043E\u044E ".concat(productTitle.value, " \u0432\u0436\u0435 \u0456\u0441\u043D\u0443\u0454!"));
             productTitle.value = name.innerText;
         }
     });
@@ -51,6 +88,9 @@ function setupProductTitle(productItemSection, productName, productId) {
 function setupCancelBtn(productItemSection, productId) {
     var cancelBtn = productItemSection.querySelector("button.cancel-btn");
     cancelBtn.addEventListener("click", function () {
+        var productName = getProductNameById(productId);
+        deleteProductNameById(productId);
+        deleteProductDataByName(productName);
         document.querySelectorAll("[data-product-id=\"".concat(productId, "\"]")).forEach(function (e) { return e.remove(); });
     });
 }
@@ -59,6 +99,10 @@ function setupBuyBtn(productItemSection, productId) {
     buyBtn.addEventListener("click", function () {
         var _a;
         var isBought = buyBtn.getAttribute("data-product-bought") === "false";
+        var productName = getProductNameById(productId);
+        var productData = getProductDataByName(productName);
+        productData.isBought = isBought;
+        setProductDataByName(productName, productData);
         buyBtn.setAttribute("data-product-bought", isBought.toString());
         buyBtn.innerText = isBought ? "Не куплено" : "Куплено";
         buyBtn.setAttribute("data-tooltip", isBought ? "Відмінити покупку" : "Підтвердити покупку");
@@ -76,24 +120,27 @@ function setupBuyBtn(productItemSection, productId) {
         parentSection.appendChild(statisticsItem);
     });
 }
-function setupAmountBtns(productItemSection, productId) {
-    var updateAmount = function (updater) {
-        var itemAmount = productItemSection.querySelector(".product-amount-value");
-        var statisticsAmount = getStatisticsItem(productId).querySelector(".amount");
-        var curAmount = parseInt(itemAmount.innerText);
-        curAmount = updater(curAmount);
-        subBtn.disabled = curAmount == 1;
-        itemAmount.innerText = curAmount.toString();
-        statisticsAmount.innerText = curAmount.toString();
-    };
+function setupAmountBtns(productItemSection, productId, amount) {
     var subBtn = productItemSection.querySelector(".btn-sub");
     subBtn.addEventListener("click", function () {
-        updateAmount(function (amount) { return --amount; });
+        updateAmount(productId, productItemSection, function (amount) { return --amount; });
     });
     var addBtn = productItemSection.querySelector(".btn-add");
     addBtn.addEventListener("click", function () {
-        updateAmount(function (amount) { return ++amount; });
+        updateAmount(productId, productItemSection, function (amount) { return ++amount; });
     });
+}
+function updateAmount(productId, productItemSection, updater) {
+    var itemAmount = productItemSection.querySelector(".product-amount-value");
+    var statisticsAmount = getStatisticsItem(productId).querySelector(".amount");
+    var productName = getProductNameById(productId);
+    var productData = getProductDataByName(productName);
+    productData.amount = updater(productData.amount);
+    setProductDataByName(productName, productData);
+    var subBtn = productItemSection.querySelector(".btn-sub");
+    subBtn.disabled = productData.amount == 1;
+    itemAmount.innerText = productData.amount.toString();
+    statisticsAmount.innerText = productData.amount.toString();
 }
 function getStatisticsItem(productId) {
     return document.querySelector(".statistics-item[data-product-id=\"".concat(productId, "\"]"));
@@ -106,11 +153,11 @@ function getProductItemFromTemplate() {
     var template = document.getElementById("product-list-item-template");
     return cloneTemplateContent(template);
 }
-function createStatisticsItem(productName, productId) {
+function createStatisticsItem(productData, productId) {
     var statisticsItem = getStatisticsItemFromTemplate();
     setProductId(statisticsItem, productId);
     var name = statisticsItem.querySelectorAll("span > span")[0];
-    name.innerText = productName;
+    name.innerText = productData.name;
     var statisticsSection = document.getElementById("not-bought-section");
     statisticsSection.appendChild(statisticsItem);
 }
@@ -124,4 +171,56 @@ function cloneTemplateContent(template) {
 }
 function setProductId(element, id) {
     element.setAttribute("data-product-id", id);
+}
+function checkIfProductExist(productName) {
+    var map = readProductDataMap();
+    return map.has(productName);
+}
+function getProductDataByName(productName) {
+    var map = readProductDataMap();
+    return map.get(productName);
+}
+function setProductDataByName(productName, productData) {
+    var map = readProductDataMap();
+    map.set(productName, productData);
+    writeProductDataMap(map);
+}
+function deleteProductDataByName(productName) {
+    var map = readProductDataMap();
+    map.delete(productName);
+    writeProductDataMap(map);
+}
+function readProductDataMap() {
+    return readMap(PRODUCT_TO_DATA_LOCAL_STORAGE_KEY);
+}
+function writeProductDataMap(productDataMap) {
+    writeMap(productDataMap, PRODUCT_TO_DATA_LOCAL_STORAGE_KEY);
+}
+function getProductNameById(productId) {
+    var map = readProductIdMap();
+    return map.get(productId);
+}
+function setProductNameById(id, name) {
+    var map = readProductIdMap();
+    map.set(id, name);
+    writeProductIdMap(map);
+}
+function deleteProductNameById(id) {
+    var map = readProductIdMap();
+    map.delete(id);
+    writeProductIdMap(map);
+}
+function readProductIdMap() {
+    return readMap(ID_TO_PRODUCT_LOCAL_STORAGE_KEY);
+}
+function writeProductIdMap(map) {
+    writeMap(map, ID_TO_PRODUCT_LOCAL_STORAGE_KEY);
+}
+function readMap(localStorageKey) {
+    var parsedArray = JSON.parse(localStorage.getItem(localStorageKey));
+    var map = new Map(parsedArray);
+    return map;
+}
+function writeMap(map, localStorageKey) {
+    localStorage.setItem(localStorageKey, JSON.stringify(Array.from(map)));
 }
--- script.ts
+++ script.ts
@@ -1,18 +1,38 @@
+const PRODUCT_TO_DATA_LOCAL_STORAGE_KEY = "productNameToData";
+const ID_TO_PRODUCT_LOCAL_STORAGE_KEY = "productIdToName";
+
+if (localStorage.getItem(PRODUCT_TO_DATA_LOCAL_STORAGE_KEY) == null) {
+    localStorage.setItem(PRODUCT_TO_DATA_LOCAL_STORAGE_KEY, "[]");
+}
+if (localStorage.getItem(ID_TO_PRODUCT_LOCAL_STORAGE_KEY) == null) {
+    localStorage.setItem(ID_TO_PRODUCT_LOCAL_STORAGE_KEY, "[]");
+}
+
+readProductIdMap().forEach((name, id) => {
+    let data = getProductDataByName(name);
+    createProduct(id, data);
+});
+
 let productAddForm = document.querySelector("form.create-product-bar")!;
 productAddForm.addEventListener("submit", (event) => {
     event.preventDefault();
     let productName = popProductName();
-    if(productName != null){
-        createProduct(productName);
+    if (productName != null) {
+        if (!checkIfProductExist(productName)) {
+            let productId = generateProductId();
+            setProductNameById(productId, productName);
+            let productData = { name: productName, amount: 1, isBought: false };
+            setProductDataByName(productName, productData);
+            createProduct(productId, productData);
+        } else {
+            alert(`Товар з назвою ${productName} вже існує!`);
+        }
     }
 });
 
-["Помідори", "Печиво", "Сир"].forEach(name => createProduct(name));
-(document.querySelector(".buy-btn") as HTMLButtonElement).click();
-
 function popProductName(): string | null {
     let input = productAddForm?.getElementsByTagName("input")[0] as HTMLInputElement;
-    if(input.value == ""){
+    if (input.value == "") {
         return null;
     }
     let productName = input.value;
@@ -20,24 +40,29 @@ function popProductName(): string | null {
     return productName;
 }
 
-function createProduct(productName: string) {
-    let productId = generateProductId(productName);
-    createProductsListItem(productName, productId);
-    createStatisticsItem(productName, productId);
+function createProduct(productId: string, productData: ProductData) {
+    let productItemSection = createProductsListItem(productData, productId);
+    createStatisticsItem(productData, productId);
+    if (productData.isBought) {
+        let buyBtn = productItemSection.querySelector(`.buy-btn`) as HTMLButtonElement;
+        buyBtn.click();
+    }
+    updateAmount(productId, productItemSection, amount => productData.amount);
 }
 
-function generateProductId(productName: string): string {
-    return `${productName}-${Math.random()}`;
+function generateProductId(): string {
+    return Math.random().toString();
 }
 
-function createProductsListItem(productName: string, productId: string) {
+function createProductsListItem(productData: ProductData, productId: string): HTMLElement {
     let productItemSection = getProductItemFromTemplate();
     setProductId(productItemSection, productId);
-    setupProductTitle(productItemSection, productName, productId);
+    setupProductTitle(productItemSection, productData.name, productId);
     setupCancelBtn(productItemSection, productId);
     setupBuyBtn(productItemSection, productId);
-    setupAmountBtns(productItemSection, productId);
+    setupAmountBtns(productItemSection, productId, productData.amount);
     addProductItemToList(productItemSection);
+    return productItemSection;
 }
 
 function setupProductTitle(productItemSection: HTMLElement, productName: string, productId: string) {
@@ -46,9 +71,21 @@ function setupProductTitle(productItemSection: HTMLElement, productName: string,
     productTitle.addEventListener("change", () => {
         let statisticsItem = getStatisticsItem(productId);
         let name = statisticsItem.firstElementChild as HTMLElement;
-        if(productTitle.value != ""){
-            name.innerText = productTitle.value;
+        if (!checkIfProductExist(productTitle.value)) {
+            if (productTitle.value != "") {
+                let newName = productTitle.value;
+                let oldName = productName;
+                setProductNameById(productId, newName);
+                let productData = getProductDataByName(oldName);
+                deleteProductDataByName(oldName);
+                productData.name = newName;
+                name.innerText = newName;
+                setProductDataByName(productData.name, productData);
+            } else {
+                productTitle.value = name.innerText;
+            }
         } else {
+            alert(`Товар з назвою ${productTitle.value} вже існує!`);
             productTitle.value = name.innerText;
         }
     });
@@ -57,6 +94,9 @@ function setupProductTitle(productItemSection: HTMLElement, productName: string,
 function setupCancelBtn(productItemSection: HTMLElement, productId: string) {
     let cancelBtn = productItemSection.querySelector("button.cancel-btn") as HTMLButtonElement;
     cancelBtn.addEventListener("click", () => {
+        let productName = getProductNameById(productId);
+        deleteProductNameById(productId);
+        deleteProductDataByName(productName);
         document.querySelectorAll(`[data-product-id="${productId}"]`).forEach(e => e.remove());
     });
 }
@@ -65,6 +105,12 @@ function setupBuyBtn(productItemSection: HTMLElement, productId: string) {
     let buyBtn = productItemSection.querySelector(".buy-btn") as HTMLButtonElement;
     buyBtn.addEventListener("click", () => {
         let isBought = buyBtn.getAttribute("data-product-bought") === "false";
+
+        let productName = getProductNameById(productId);
+        let productData = getProductDataByName(productName);
+        productData.isBought = isBought;
+        setProductDataByName(productName, productData);
+
         buyBtn.setAttribute("data-product-bought", isBought.toString());
         buyBtn.innerText = isBought ? "Не куплено" : "Куплено";
         buyBtn.setAttribute("data-tooltip", isBought ? "Відмінити покупку" : "Підтвердити покупку");
@@ -87,29 +133,32 @@ function setupBuyBtn(productItemSection: HTMLElement, productId: string) {
     });
 }
 
-function setupAmountBtns(productItemSection: HTMLElement, productId: string) {
-    let updateAmount = (updater: (amount: number) => number) => {
-        let itemAmount = productItemSection.querySelector(".product-amount-value") as HTMLElement;
-        let statisticsAmount = getStatisticsItem(productId).querySelector(".amount") as HTMLElement;
-        let curAmount = parseInt(itemAmount.innerText);
-        curAmount = updater(curAmount);
-        subBtn.disabled = curAmount == 1;
-        itemAmount.innerText = curAmount.toString();
-        statisticsAmount.innerText = curAmount.toString();
-    }
-
+function setupAmountBtns(productItemSection: HTMLElement, productId: string, amount: number) {
     let subBtn = productItemSection.querySelector(".btn-sub") as HTMLButtonElement;
     subBtn.addEventListener("click", () => {
-        updateAmount(amount => --amount);
+        updateAmount(productId, productItemSection, amount => --amount);
     });
 
     let addBtn = productItemSection.querySelector(".btn-add") as HTMLButtonElement;
     addBtn.addEventListener("click", () => {
-        updateAmount(amount => ++amount);
+        updateAmount(productId, productItemSection, amount => ++amount);
     });
 }
 
-function getStatisticsItem(productId: string) : HTMLElement {
+function updateAmount(productId: string, productItemSection: HTMLElement, updater: (amount: number) => number) {
+    let itemAmount = productItemSection.querySelector(".product-amount-value") as HTMLElement;
+    let statisticsAmount = getStatisticsItem(productId).querySelector(".amount") as HTMLElement;
+    let productName = getProductNameById(productId);
+    let productData = getProductDataByName(productName);
+    productData.amount = updater(productData.amount);
+    setProductDataByName(productName, productData);
+    let subBtn = productItemSection.querySelector(".btn-sub") as HTMLButtonElement;
+    subBtn.disabled = productData.amount == 1;
+    itemAmount.innerText = productData.amount.toString();
+    statisticsAmount.innerText = productData.amount.toString();
+}
+
+function getStatisticsItem(productId: string): HTMLElement {
     return document.querySelector(`.statistics-item[data-product-id="${productId}"]`) as HTMLElement;
 }
 
@@ -123,11 +172,11 @@ function getProductItemFromTemplate(): HTMLElement {
     return cloneTemplateContent(template);
 }
 
-function createStatisticsItem(productName: string, productId: string) {
+function createStatisticsItem(productData: ProductData, productId: string) {
     let statisticsItem = getStatisticsItemFromTemplate();
     setProductId(statisticsItem, productId);
     let name = (statisticsItem.querySelectorAll("span > span")[0] as HTMLElement);
-    name.innerText = productName;
+    name.innerText = productData.name;
     let statisticsSection = document.getElementById("not-bought-section") as HTMLElement;
     statisticsSection.appendChild(statisticsItem);
 }
@@ -143,4 +192,75 @@ function cloneTemplateContent(template: HTMLTemplateElement): HTMLElement {
 
 function setProductId(element: HTMLElement, id: string) {
     element.setAttribute("data-product-id", id);
+}
+
+type ProductData = {
+    name: string;
+    amount: number;
+    isBought: boolean;
+}
+
+function checkIfProductExist(productName: string): boolean {
+    let map: Map<string, ProductData> = readProductDataMap();
+    return map.has(productName);
+}
+
+function getProductDataByName(productName: string): ProductData {
+    let map = readProductDataMap();
+    return map.get(productName)!;
+}
+
+function setProductDataByName(productName: string, productData: ProductData) {
+    let map = readProductDataMap();
+    map.set(productName, productData);
+    writeProductDataMap(map);
+}
+
+function deleteProductDataByName(productName: string) {
+    let map = readProductDataMap();
+    map.delete(productName);
+    writeProductDataMap(map);
+}
+
+function readProductDataMap(): Map<string, ProductData> {
+    return readMap<string, ProductData>(PRODUCT_TO_DATA_LOCAL_STORAGE_KEY);
+}
+
+function writeProductDataMap(productDataMap: Map<string, ProductData>) {
+    writeMap(productDataMap, PRODUCT_TO_DATA_LOCAL_STORAGE_KEY);
+}
+
+function getProductNameById(productId: string) : string {
+    let map = readProductIdMap();
+    return map.get(productId)!;
+}
+
+function setProductNameById(id: string, name: string) {
+    let map = readProductIdMap();
+    map.set(id, name);
+    writeProductIdMap(map);
+}
+
+function deleteProductNameById(id: string) {
+    let map = readProductIdMap();
+    map.delete(id);
+    writeProductIdMap(map);
+}
+
+function readProductIdMap(): Map<string, string> {
+    return readMap<string, string>(ID_TO_PRODUCT_LOCAL_STORAGE_KEY);
+}
+
+function writeProductIdMap(map: Map<string, string>){
+    writeMap(map, ID_TO_PRODUCT_LOCAL_STORAGE_KEY);
+}
+
+function readMap<TKey, TValue>(localStorageKey: string) : Map<TKey, TValue> {
+    let parsedArray: [TKey, TValue][] = JSON.parse(localStorage.getItem(localStorageKey)!);
+    let map = new Map<TKey, TValue>(parsedArray);
+    return map;
+}
+
+function writeMap<TKey, TValue>(map: Map<TKey, TValue>, localStorageKey: string) {
+    localStorage.setItem(localStorageKey, JSON.stringify(Array.from(map)));
 }
